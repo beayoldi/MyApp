d7dbcbad6bcede46f08072acc8d32137b94e5c20
100383359 <100383359@alumnos.uc3m.es>
2021-02-22 18:28:43 +0100

    Cosas de la primera clase

---
+++ app/src/main/java/com/example/myapp/models/UserDao.kt
@@ -0,0 +1,15 @@
+package com.example.myapp.models
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Insert (onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addUser(user: User)
+    @Query("SELECT * FROM user_table ORDER BY id ASC")
+    fun readAll(): LiveData<List<User>>
+}
