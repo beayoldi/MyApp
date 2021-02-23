598831cef138244fdf7f9c294a167083e70808fd
100383359 <100383359@alumnos.uc3m.es>
2021-02-23 17:36:04 +0100

    Cambios

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.myapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+enum class ProviderType{
+    GOOGLE
+}
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
