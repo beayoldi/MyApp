359d7a9b00e281369e2914cb80fc517d3a030fd9
100383359 <100383359@alumnos.uc3m.es>
2021-02-27 18:07:33 +0100

    Cosas

--- app/src/main/java/com/example/myapp/views/Fragment1.kt
+++ app/src/main/java/com/example/myapp/views/Fragment1.kt
@@ -1,22 +1,54 @@
 package com.example.myapp.views
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.myapp.R
+import com.example.myapp.databinding.Fragment1Binding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 
 class Fragment1 : Fragment() {
+    private lateinit var binding: Fragment1Binding
+    private lateinit var mAuth: FirebaseAuth
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
+        binding = Fragment1Binding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        val signIn = binding.signInButton
+        signIn.setOnClickListener{
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
+            var mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        }
+
+    }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth.currentUser
+        //updateUI(currentUser)
+    }
+
+
+
 
 }
